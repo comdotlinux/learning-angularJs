2734e3da42485885c8bab7eeb5999d95b36676c9
comdotlinux <guru@linux.com>
2016-01-23 01:05:59 +0530

    new directives

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var angularApp = angular.module('angularApp', []);
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope', function ($scope) {
     $scope.name = "James";
+
+    $scope.allowedCharacters = 10;
 }]);
 
 
