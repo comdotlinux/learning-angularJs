2330882c248603b8551abc4de9d7d8e834eab6cc
Guruprasad Kulkarni <guru@linux.com>
2016-03-01 17:11:38 +0000

    Updates

--- app.js
+++ app.js
@@ -2,31 +2,32 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$log', function ($scope, $log) {
-    
+angularApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+
     $scope.userHandle = {
         handle: "",
         hideHandleInputField: false,
-        handleUpdated: function () {
+        handleUpdated: function() {
             $scope.userHandle.hideHandleInputField = true;
         }
     };
-    
+
     $scope.nameHandler = {
         name: "",
         allowedCharacters: 10,
         progressPercent: 0,
         nameInvalid: "",
         nameValid: "",
-        handleNameUpdated: function () {
+        handleNameUpdated: function() {
             if ($scope.nameHandler.name.length < $scope.nameHandler.allowedCharacters) {
                 $scope.nameHandler.nameInvalid = "alert alert-warning";
                 $scope.nameHandler.nameValid = "";
-            } else {
+            }
+            else {
                 $scope.nameHandler.nameValid = "alert alert-success";
                 $scope.nameHandler.nameInvalid = "";
             }
-        
+
             $scope.nameHandler.progressPercent = (($scope.nameHandler.name.length * 100) / $scope.nameHandler.allowedCharacters);
             if ($scope.nameHandler.progressPercent > 100) {
                 $log.info("name length is greater than or equal to 100");
